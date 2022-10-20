4ebb84bcd91ce1b25e8df01fc035c4bd4e4b36a5
Blaine Powell <brpowel4@ncsu.edu>
2022-10-20 00:49:11 -0400

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   
   return (
     <>
-    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-DWE6NJTWCJ"/>
     <Script
       id='google-analytics'
       strategy="afterInteractive"
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-XXXXXXX', {
+          gtag('config', 'G-DWE6NJTWCJ', {
             page_path: window.location.pathname,
           });
         `,
