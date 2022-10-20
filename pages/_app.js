30cc5d8c976a18eda50576bd3430dfdc7d84bc49
Blaine Powell <brpowel4@ncsu.edu>
2022-10-20 00:51:58 -0400

    one more time

--- pages/_app.js
+++ pages/_app.js
@@ -5,21 +5,14 @@ function MyApp({ Component, pageProps }) {
   
   return (
     <>
-    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-DWE6NJTWCJ"/>
-    <Script
-      id='google-analytics'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-DWE6NJTWCJ', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-    />
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DWE6NJTWCJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DWE6NJTWCJ');
+</script>
    <Component {...pageProps} />
    </>
   )
